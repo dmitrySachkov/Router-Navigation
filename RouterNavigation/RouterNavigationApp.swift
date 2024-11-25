7149d100a2d637ad3fa617be74c65855ff8f2f46
domont <d.a.sachkov@gmail.com>
2024-11-25 09:26:57 +0300

    Initial Commit

---
+++ RouterNavigation/RouterNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterNavigationApp.swift
+//  RouterNavigation
+//
+//  Created by Dmitry Sachkov on 25.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RouterNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
